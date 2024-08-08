42d307a48e77aa4774c14656aed88c5d50f802bf
Eric Yew <eric9yew@gmail.com>
2024-08-09 02:06:12 +0800

    add ngSrc

--- src/app/component/home/home.component.ts
+++ src/app/component/home/home.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import {HeaderComponent} from "../header/header.component";
 import {FooterComponent} from "../footer/footer.component";
+import {NgOptimizedImage} from "@angular/common";
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    NgOptimizedImage
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
